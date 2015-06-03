73fe923b40abc70a2d877855b1345544c854e889
Patricio Bruna <pbruna@gmail.com>
2015-06-03 07:18:18 -0300

    Fixed params[:q]

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -10,5 +10,13 @@ module MessagesHelper
   def display_message_to(message)
     truncate(message.to.join(", "), length: 100)
   end
+  
+  def display_msg_type(message)
+    if message.dte
+      return "DTE #{message.dte.msg_type.upcase}" unless message.dte.msg_type.nil?
+    else
+      return "DTE"
+    end
+  end
 
 end
