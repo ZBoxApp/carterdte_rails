dd251ecad60d297a689248c3dc71e8943bf6242e
Patricio Bruna <pbruna@gmail.com>
2015-11-11 08:22:39 -0300

    Fixed timezone

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,17 @@
+# Docs
 module ApplicationHelper
-  
   def active_menu?(controller)
-    "active" if params[:controller] == controller
+    'active' if params[:controller] == controller
   end
-  
+
   def dte_page_link(page)
     new_params = params.merge(page: page)
     new_params.delete('action')
     new_params.delete('controller')
     dte_messages_path(new_params)
   end
-  
+
+  def app_time_zone
+    Rails.application.config.time_zone
+  end
 end
